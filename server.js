2857214b63b8543889ad6d1a0e4840d8a6fb5cfb
Natnael31 <end24002byui.edu>
2025-05-26 21:59:48 +0300

    added login

--- server.js
+++ server.js
@@ -11,9 +11,36 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
 const baseController = require('./controllers/baseController');
 const utilities = require("./utilities");
 const invController = require("./controllers/invController");
+const session = require("express-session")
+const pool = require('./database/')
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 /* ***********************
  * View Engine and Templates
@@ -28,6 +55,8 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 app.use("/inv", inventoryRoute);
+app.use("/account", accountRoute);
+// app.use("/account", require("./routes/accountRoute"));
 
 // Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
